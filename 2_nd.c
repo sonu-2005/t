5d65e1f3661ba6c0bf8e38ee52429728703d7caf
sonu-2005 <spandanaprakash11@gmail.com>
2026-04-14 18:31:42 +0530

    Update 2_nd.c

--- 2_nd.c
+++ 2_nd.c
@@ -1,62 +1,66 @@
 #include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
 
-int main()
+void main()
 {
-    int month[12]={31,28,31,30,31,30,31,31,30,31,30,31};
-    int d,m,y,nd,nm,ny,ndays;
+int month[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+int d,m,y,nd,nm,ny,ndays;
 
-    printf("enter the date,month,year");
-    scanf("%d%d%d",&d,&m,&y);
+printf("enter the date,month,year");
+scanf("%d%d%d",&d,&m,&y);
 
-    if(y<=1812 || y>2012)
-    {
-        printf("Invalid Input Year");
-        return 0;
-    }
+if(y<=1812 || y>2012)
+{
+printf("Invalid Input Year");
+getch();
+exit(0);
+}
 
-    if(m<1 || m>12)
-    {
-        printf("Invalid Input Month");
-        return 0;
-    }
+if(m<1 || m>12)
+{
+printf("Invalid Input Month");
+getch();
+exit(0);
+}
 
-    ndays=month[m-1];
+ndays=month[m-1];
 
-    if(m==2)
-    {
-        if(y%100==0)
-        {
-            if(y%400==0)
-                ndays=29;
-        }
-        else if(y%4==0)
-            ndays=29;
-    }
+if(m==2)
+{
+ if(y%100==0)
+ {
+  if(y%400==0)
+   ndays=29;
+ }
+ else if(y%4==0)
+  ndays=29;
+}
 
-    if(d<=0 || d>ndays)
-    {
-        printf("Invalid Input Day");
-        return 0;
-    }
+if(d<=0 || d>ndays)
+{
+printf("Invalid Input Day");
+getch();
+exit(0);
+}
 
-    nd=d+1;
-    nm=m;
-    ny=y;
+nd=d+1;
+nm=m; ny=y;
 
-    if(nd>ndays)
-    {
-        nd=1;
-        nm++;
-    }
+if(nd>ndays)
+{
+nd=1;
+nm++;
+}
 
-    if(nm>12)
-    {
-        nm=1;
-        ny++;
-    }
+if(nm>12)
+{
+nm=1;
+ny++;
+}
 
-    printf("\n Given date is %d:%d:%d",d,m,y);
-    printf("\n Next day’s date is %d:%d:%d",nd,nm,ny);
+printf("\n Given date is %d:%d:%d",d,m,y);
+printf("\n Next dayâ€™s date is %d:%d:%d",nd,nm,ny);
 
-    return 0;
+getch();
 }
